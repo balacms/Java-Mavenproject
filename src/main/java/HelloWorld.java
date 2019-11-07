0c1fffa87c6fded4ccc7cab9154ed104ea74f998
balacms <37127800+balacms@users.noreply.github.com>
2019-11-06 18:18:55 -0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World- Devops Training v2.1");
+		get("/hello", (req, res) -> "Hello World- Devops Training v2.2");
 	}
 }
